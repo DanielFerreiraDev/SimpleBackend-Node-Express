b7a15b8b883b5fdb036c837f17556bba95c6bc30
Daniel Nascimento <danielnascimento@mobitbrasil.com.br>
2023-02-14 14:24:47 -0300

    Adicionando extensão para dar suporte ao APM agent do express

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,28 @@
-const express = require('express');
+// Add this to the VERY top of the first file loaded in your app
+const apm = require('elastic-apm-node').start({
+    // Override service name from package.json
+    // Allowed characters: a-z, A-Z, 0-9, -, _, and space
+    serviceName: 'apm-express',
 
-const app = express();
+    // Use if APM Server requires a token
+    secretToken: '',
+
+    // Use if APM Server uses API keys for authentication
+    apiKey: '',
+
+    // Set custom APM Server URL (default: http://localhost:8200)
+    serverUrl: '',
+})
+
+const app = require('express')()
+
+var err = new Error('Ups, something broke!')
+
+apm.captureError(err)
+
+/*const express = require('express');
+
+const app = express();*/
 
 app.get('/', function (req, res) {
     res.send('Bem-vindo!')
